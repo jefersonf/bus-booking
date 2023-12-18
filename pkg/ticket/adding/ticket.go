d8ee9fceefc77d4a10a7dc72afab3ba5c55de625
jeferson <jefersonnpn@gmail.com>
2023-12-18 16:42:08 -0300

    :wrench: fix & add price field

--- pkg/ticket/adding/ticket.go
+++ pkg/ticket/adding/ticket.go
@@ -2,10 +2,11 @@ package adding
 
 // Ticket decribes the properties of a ticket to be added.
 type Ticket struct {
-	OriginID           string `json:"origin_id"`
-	DestinatioID       string `json:"destination"`
-	PurchasingLocation string `json:"purchasing_location"`
-	PassengerID        int    `json:"passenger_id"`
-	PassengerName      string `json:"passenger_name"`
-	SeatNumber         int    `json:"seat_number"`
+	OriginID           string  `json:"origin_id"`
+	DestinationID      string  `json:"destination_id"`
+	PurchasingLocation string  `json:"purchasing_location"`
+	PassengerID        int     `json:"passenger_id"`
+	PassengerName      string  `json:"passenger_name"`
+	SeatNumber         int     `json:"seat_number"`
+	Price              float32 `json:"price"`
 }
