9183f0f7e8948bca37a2f453ded95be7d5f7e7e8
jeferson <jefersonnpn@gmail.com>
2023-12-21 18:19:38 -0300

    :sparkles:  baseURL endpoint

--- internal/http/rest/handlers.go
+++ internal/http/rest/handlers.go
@@ -1,16 +1,17 @@
 package rest
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func handlers() *http.ServeMux {
 	routes := http.NewServeMux()
 
-	routes.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "fleet_size=%v", 10)
-	})
+	routes.HandleFunc("/", baseURI)
 
 	return routes
+}
+
+func baseURI(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status": "running"}`))
 }
