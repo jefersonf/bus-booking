8cbc230b7ea3bff21eecb83503b5f5d011de6afd
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-01-10 19:40:12 -0300

    fix typo and and json tag for estimated travel time

--- pkg/trip/overview/trip.go
+++ pkg/trip/overview/trip.go
@@ -2,9 +2,9 @@ package overview
 
 import "time"
 
-// Trip describes the properties of a triup overview.
+// Trip describes the properties of a trip overview.
 type Trip struct {
-	Origin              string `json:"departing_from"`
-	Destination         string `json:"going_to"`
-	EstimatedTravelTime time.Duration
+	Origin              string        `json:"departing_from"`
+	Destination         string        `json:"going_to"`
+	EstimatedTravelTime time.Duration `json:"ett"`
 }
