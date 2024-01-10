79b5b9af8df30cd43578cdb9892aec45beb2ac59
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-01-09 23:43:46 -0300

    Update passenger.go

--- pkg/identity/passenger.go
+++ pkg/identity/passenger.go
@@ -4,8 +4,8 @@ type IdentityType string
 
 // Identity describes the properties of a single passenger identity type.
 type Identity struct {
-	Description  string `json:"description"`
-	Value        string `json:"value"`
+	Description  string       `json:"description"`
+	Value        string       `json:"value"`
 	Type         IdentityType `json:"type"`
 
 }
