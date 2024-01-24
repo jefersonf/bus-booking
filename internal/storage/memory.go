ef738fc3bb81c1f66d9fbcc7c1dca46d74abe966
jeferson <jefersonnpn@gmail.com>
2024-01-24 19:50:19 -0300

    :bus: memorize buses on memory

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -1,7 +1,11 @@
 package storage
 
-import "github.com/jefersonf/bus-booking/pkg/ticket/listing"
+import (
+	busListing "github.com/jefersonf/bus-booking/pkg/bus/listing"
+	ticketListing "github.com/jefersonf/bus-booking/pkg/ticket/listing"
+)
 
 type MemoryStorage struct {
-	tickets map[string]listing.Ticket
+	tickets map[string]ticketListing.Ticket
+	buses   map[string]busListing.Bus
 }
