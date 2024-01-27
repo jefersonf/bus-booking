7d432bdd6e72ca27e9b32fc802d0d98813180214
jeferson <jefersonnpn@gmail.com>
2024-01-26 21:33:30 -0300

    :memo: add doc

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -5,6 +5,7 @@ import (
 	ticketListing "github.com/jefersonf/bus-booking/pkg/ticket/listing"
 )
 
+// MemoryStorage represents a memory storage of tickets and buses.
 type MemoryStorage struct {
 	tickets map[string]ticketListing.Ticket
 	buses   map[string]busListing.Bus
