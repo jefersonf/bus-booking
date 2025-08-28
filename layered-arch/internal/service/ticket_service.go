1bf0739e9c2104fcc11481d016232a3b775b79b6
jeferson <jefersonnpn@gmail.com>
2025-08-28 13:59:21 -0300

    :sparkles: add basic use cases on ticket service

---
+++ layered-arch/internal/service/ticket_service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+
+	"github.com/jefersonf/bus-booking/layered-arch/internal/domain"
+	"github.com/jefersonf/bus-booking/layered-arch/internal/repository"
+)
+
+type TicketService struct {
+	tripRepo   repository.TripRepository
+	ticketRepo repository.TicketRepository
+}
+
+// NewTicketService creates a new instance of TicketService.
+func NewTicketService(tripRepo repository.TripRepository, ticketRepo repository.TicketRepository) *TicketService {
+	return &TicketService{
+		tripRepo:   tripRepo,
+		ticketRepo: ticketRepo,
+	}
+}
+
+// ListAvailableTrips lists available trips based on origin and destination.
+func (s *TicketService) ListAvailableTrips(ctx context.Context, origin, destination string) ([]domain.Trip, error) {
+	return s.tripRepo.FindAvailableTrips(ctx, origin, destination)
+}
+
+func (s *TicketService) PurchaseTicket(ctx context.Context, tripID, passengerID, seatNumber string) (*domain.Ticket, error) {
+	// TODO 1. Validate trip existence and seat availability.
+	// TODO 2. Process payment
+	// TODO 3. Create and store the ticket.
+	// TODO 4. Deduct the seat from available seats.
+	// TODO 5. Return the ticket details.
+	return nil, nil
+}
